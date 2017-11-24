1c64094fc89efcb9a71675eec17f35b49f7ba255
nguyen van co <33902471+nguyenvanco123@users.noreply.github.com>
2017-11-24 19:49:07 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://www.facebook.com/nguyenlieutre/'>https://www.facebook.com/nguyenlieutre</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='nguyenvanco123/nguyenvanco123/upload'>nguyenvanco123/nguyenvanco123/upload</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
